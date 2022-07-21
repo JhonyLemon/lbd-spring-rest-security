da0286bdd2f257840e3468c9934ef1faf6606e60
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-21 09:00:13 +0200

    Initial commit

---
+++ src/main/java/pl/fissst/lbd/restsecurity/RestSecurityApplication.java
@@ -0,0 +1,13 @@
+package pl.fissst.lbd.restsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSecurityApplication.class, args);
+    }
+
+}
