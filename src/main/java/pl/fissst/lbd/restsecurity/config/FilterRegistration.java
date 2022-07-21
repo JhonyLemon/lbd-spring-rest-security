cf8989d5c504a970979f5ef17cf5053a92485bb3
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-21 11:11:47 +0200

    Task 19

--- src/main/java/pl/fissst/lbd/restsecurity/config/FilterRegistration.java
+++ src/main/java/pl/fissst/lbd/restsecurity/config/FilterRegistration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.fissst.lbd.restsecurity.filters.AuthorizationStudentFilter;
+import pl.fissst.lbd.restsecurity.filters.AuthorizationTeacherFilter;
 
 @Configuration
 public class FilterRegistration {
@@ -15,6 +16,18 @@ public class FilterRegistration {
 
         registration.setFilter(new AuthorizationStudentFilter());
         registration.addUrlPatterns("/api/student/*");
+        registration.setOrder(2);
+
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthorizationTeacherFilter> authorizationTeacherFilter(){
+        FilterRegistrationBean<AuthorizationTeacherFilter> registration
+                = new FilterRegistrationBean<>();
+
+        registration.setFilter(new AuthorizationTeacherFilter());
+        registration.addUrlPatterns("/api/teacher/*");
         registration.setOrder(2);
 
         return registration;
---
+++ src/main/java/pl/fissst/lbd/restsecurity/filters/AuthorizationTeacherFilter.java
@@ -0,0 +1,38 @@
+package pl.fissst.lbd.restsecurity.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorizationTeacherFilter implements Filter {
+
+    private static final String newContent = "{ \"errorMessage\" : \"User unauthorized!\" }";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        doFilter((HttpServletRequest) servletRequest,(HttpServletResponse) servletResponse,filterChain);
+    }
+    private void doFilter(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws IOException, ServletException {
+            String role=httpServletRequest.getHeader("role");
+            if(role==null || !role.equals("TEACHER_ROLE")) {
+                httpServletResponse.resetBuffer();
+                httpServletResponse.setStatus(401);
+                httpServletResponse.getOutputStream().print(newContent);
+                httpServletResponse.getOutputStream().flush();
+                httpServletResponse.flushBuffer();
+                return;
+            }
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
