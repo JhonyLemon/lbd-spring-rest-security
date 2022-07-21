bf3f70bc98af2938f20796785021c224d9372572
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-21 09:31:01 +0200

    Task 6

--- src/main/java/pl/fissst/lbd/restsecurity/controllers/StudentController.java
+++ src/main/java/pl/fissst/lbd/restsecurity/controllers/StudentController.java
@@ -1,9 +1,6 @@
 package pl.fissst.lbd.restsecurity.controllers;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.fissst.lbd.restsecurity.dto.Student;
 import pl.fissst.lbd.restsecurity.services.StudentService;
 
@@ -24,5 +21,26 @@ public class StudentController {
     public List<Student> getAllStudent(){
 
         return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student){
+        return studentService.insertStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student editStudent(
+            @PathVariable("id") Long id,
+            @RequestParam("age") Integer age,
+            @RequestParam("lastName") String lastName){
+        return studentService.editStudent(id,age,lastName);
+    }
+    @DeleteMapping("/{id}")
+    public Boolean deleteStudent(@PathVariable("id") Long id){
+        return studentService.deleteStudent(id);
+    }
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable("id") Long id){
+        return studentService.getStudent(id);
     }
 }
