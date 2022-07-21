918ffcfed0876c5defd6a38a2b25b5575ade4819
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-21 11:14:26 +0200

    Task 21

--- src/main/java/pl/fissst/lbd/restsecurity/config/FilterRegistration.java
+++ src/main/java/pl/fissst/lbd/restsecurity/config/FilterRegistration.java
@@ -9,28 +9,29 @@ import pl.fissst.lbd.restsecurity.filters.AuthorizationTeacherFilter;
 @Configuration
 public class FilterRegistration {
 
-    @Bean
-    public FilterRegistrationBean<AuthorizationStudentFilter> authorizationStudentFilter(){
-        FilterRegistrationBean<AuthorizationStudentFilter> registration
-                = new FilterRegistrationBean<>();
+//    @Bean
+//    public FilterRegistrationBean<AuthorizationStudentFilter> authorizationStudentFilter(){
+//        FilterRegistrationBean<AuthorizationStudentFilter> registration
+//                = new FilterRegistrationBean<>();
+//
+//        registration.setFilter(new AuthorizationStudentFilter());
+//        registration.addUrlPatterns("/api/student/*");
+//        registration.setOrder(2);
+//
+//        return registration;
+//    }
+//    @Bean
+//    public FilterRegistrationBean<AuthorizationTeacherFilter> authorizationTeacherFilter(){
+//        FilterRegistrationBean<AuthorizationTeacherFilter> registration
+//                = new FilterRegistrationBean<>();
+//
+//        registration.setFilter(new AuthorizationTeacherFilter());
+//        registration.addUrlPatterns("/api/teacher/*");
+//        registration.setOrder(2);
+//
+//        return registration;
+//    }
 
-        registration.setFilter(new AuthorizationStudentFilter());
-        registration.addUrlPatterns("/api/student/*");
-        registration.setOrder(2);
 
-        return registration;
-    }
-
-    @Bean
-    public FilterRegistrationBean<AuthorizationTeacherFilter> authorizationTeacherFilter(){
-        FilterRegistrationBean<AuthorizationTeacherFilter> registration
-                = new FilterRegistrationBean<>();
-
-        registration.setFilter(new AuthorizationTeacherFilter());
-        registration.addUrlPatterns("/api/teacher/*");
-        registration.setOrder(2);
-
-        return registration;
-    }
 
 }
