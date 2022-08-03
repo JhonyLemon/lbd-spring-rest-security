69085906b0847740c69f4a884dbc5b2d2f1c3450
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-08-03 11:43:10 +0200

    Changed how spring security is configured to remove usage of deprecated class

--- src/main/java/pl/fissst/lbd/restsecurity/security/SecurityConfig.java
+++ src/main/java/pl/fissst/lbd/restsecurity/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package pl.fissst.lbd.restsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import pl.fissst.lbd.restsecurity.services.MyUserDetailsService;
 
@@ -15,7 +20,7 @@ import static pl.fissst.lbd.restsecurity.security.Permissions.*;
 @EnableWebMvc
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
     private final MyUserDetailsService userDetailsService;
 
@@ -26,9 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers(HttpMethod.PUT,"/api/decimal/**")
@@ -40,12 +50,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/user/**")
                 .hasAnyAuthority(ACCESS_ALL.name())
                 .and()
-                .httpBasic()
-        ;
+                .httpBasic();
+        return http.build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
     }
+
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+//        return auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder).and().build();
+//    }
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+//    }
 }
